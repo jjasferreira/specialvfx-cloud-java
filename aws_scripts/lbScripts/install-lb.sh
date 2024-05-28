8553e331b1c14c43aef3514ce69f5565b0e887ea
José João Ferreira <josejoaoferreira@tecnico.ulisboa.pt>
2024-05-28 18:21:32 +0000

    Fix paths used to transfer files to instances

--- aws_scripts/lbScripts/install-lb.sh
+++ aws_scripts/lbScripts/install-lb.sh
@@ -7,7 +7,7 @@ cmd="sudo yum update -y; sudo yum install java-11-amazon-corretto.x86_64 -y"
 ssh -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAR_PATH ec2-user@$(cat lbScripts/instance.dns) $cmd
 
 # Install web server.
-scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAR_PATH $DIR/../cnv-shared/cnv24-g33/as_lb/webserver/target/cnv-1.0.0-SNAPSHOT.jar ec2-user@$(cat lbScripts/instance.dns):
+scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAR_PATH $DIR/../as_lb/webserver/target/cnv-1.0.0-SNAPSHOT.jar ec2-user@$(cat lbScripts/instance.dns):
 
 # Setup web server to start on instance launch.
 cmd="echo \"java -cp /home/ec2-user/cnv-1.0.0-SNAPSHOT.jar pt.ulisboa.tecnico.cnv.as_lb_server.AS_LB_Server\" | sudo tee -a /etc/rc.local; sudo chmod +x /etc/rc.local"
