681e49a11aa704d06a0427ac7d4e949f2b9bad7b
João Miguel Matias Vieira <joao.miguel.m.vieira@tecnico.ulisboa.pt>
2024-05-31 01:03:40 +0100

    Added scp of javassist wrapper and new start command for the webserver

--- aws-scripts/worker-scripts/install-worker.sh
+++ aws-scripts/worker-scripts/install-worker.sh
@@ -8,8 +8,10 @@ ssh -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAR_PATH ec2-user@$(cat worke
 
 # Install web server.
 scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAR_PATH $DIR/../specialvfx/webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar ec2-user@$(cat worker-scripts/instance.dns):
+scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAR_PATH $DIR/../specialvfx/tooling/target/JavassistWrapper-1.0-jar-with-dependencies.jar ec2-user@ec2-54-242-204-249.compute-1.amazonaws.com:
 
 # Setup web server to start on instance launch.
-cmd="echo \"java -cp /home/ec2-user/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar pt.ulisboa.tecnico.cnv.webserver.WebServer\" | sudo tee -a /etc/rc.local; sudo chmod +x /etc/rc.local"
+#cmd="echo \"java -cp /home/ec2-user/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar pt.ulisboa.tecnico.cnv.webserver.WebServer\" | sudo tee -a /etc/rc.local; sudo chmod +x /etc/rc.local"
+cmd="echo \"java -cp /home/ec2-user/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar -javaagent:/home/ec2-user/JavassistWrapper-1.0-jar-with-dependencies.jar=ICount:pt.ulisboa.tecnico.cnv.raytracer,pt.ulisboa.tecnico.cnv.imageproc,boofcv:output pt.ulisboa.tecnico.cnv.webserver.WebServer\" | sudo tee -a /etc/rc.local; sudo chmod +x /etc/rc.local"
 ssh -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAR_PATH ec2-user@$(cat worker-scripts/instance.dns) $cmd
 
